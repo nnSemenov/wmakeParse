f288c6752a66f6d9a89a64908c87ef90448123c2
ToKiNoBug <tokinobug@163.com>
2026-05-29 11:13:08 +0800

    Add version flag to parser

--- parser_exe.cpp
+++ parser_exe.cpp
@@ -39,22 +39,15 @@ int run_task(const task_info& task);
 int main(int argc, char** argv) {
   task_info task;
 
-  CLI::App app{"Parse wmake file", "parse-wmake"};
-  app.set_config("", "",
-                 "Parse single wmake file and print required information. If "
-                 "not specified, print everything.");
-  app.add_option("file", task.filename, "File to parse")
-      ->required()
-      ->check(CLI::ExistingFile);
-  app.add_flag("--print-files", task.print_files,
-               "Print files (usually source files)");
-  app.add_flag("--print-all-variables", task.print_all_vars,
-               "Print all variables (with name)");
+  CLI::App app{"Parse single wmake file and print required information. If not specified, print everything.",
+               "parse-wmake"};
+  app.set_version_flag("-v,--version", WMAKE_PARSE_VERSION);
+  app.add_option("file", task.filename, "File to parse")->required()->check(CLI::ExistingFile);
+  app.add_flag("--print-files", task.print_files, "Print files (usually source files)");
+  app.add_flag("--print-all-variables", task.print_all_vars, "Print all variables (with name)");
   app.add_option("--get", task.get_variable_name, "Get variable name");
-  app.add_flag("--parse-brace-expression", task.parse_brace_expression,
-               "Parse ${} expression");
-  app.add_flag("--strict", task.strict,
-               "Throw exception when facing referencing undefined variable");
+  app.add_flag("--parse-brace-expression", task.parse_brace_expression, "Parse ${} expression");
+  app.add_flag("--strict", task.strict, "Throw exception when facing referencing undefined variable");
 
   CLI11_PARSE(app, argc, argv);
 
@@ -74,8 +67,7 @@ int run_task(const task_info& task) {
   Foam::wmakeParse::wmake_parse_option option;
   option.parse_brace_expression = task.parse_brace_expression;
   if (task.strict) {
-    option.when_undefined_reference =
-        Foam::wmakeParse::undefined_reference_behavior::throw_exception;
+    option.when_undefined_reference = Foam::wmakeParse::undefined_reference_behavior::throw_exception;
   }
 
   auto env = Foam::wmakeParse::get_environment_variables();
@@ -85,8 +77,7 @@ int run_task(const task_info& task) {
   try {
     files = Foam::wmakeParse::parse_wmake_file(file_content, new_vars, option);
   } catch (const std::exception& e) {
-    std::println(std::cerr, "Failed to parse {}: {}", task.filename.c_str(),
-                 e.what());
+    std::println(std::cerr, "Failed to parse {}: {}", task.filename.c_str(), e.what());
     return 2;
   }
 
@@ -118,8 +109,7 @@ int run_task(const task_info& task) {
   if (not task.get_variable_name.empty()) {
     auto it = new_vars.find(task.get_variable_name);
     if (it == new_vars.end()) {
-      std::println(std::cerr, "{} doesn't contains variable \"{}\"",
-                   task.filename.c_str(), task.get_variable_name);
+      std::println(std::cerr, "{} doesn't contains variable \"{}\"", task.filename.c_str(), task.get_variable_name);
       return 1;
     }
     std::println("{}", it->second);
